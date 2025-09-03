a9569515b68f8b3d2e5cffa99a2ff2ee4ce1f20a
Hassan <hassanidrees633@gmail.com>
2025-09-02 22:50:36 -0700

    Create Program.cs

---
+++ Simple Login with ASP.Net MVC/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// If using Identity
+// builder.Services.AddAuthentication();
+// builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// If using Identity
+// app.UseAuthentication();
+// app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
